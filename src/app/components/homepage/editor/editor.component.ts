44db0f03f66ce8ef75f75e78642fdeb9071569d8
Faisal Mahmood <faisal.mahmood@zaxiss.com>
2022-01-20 17:03:27 +0000

    Merged in SIT-7-implement-tree-view-functionality-tree-completion (pull request #15)
    SIT-7 implement tree view functionality tree completion
    
    * Text selection in progress
    
    * word selection and segment header selection done. output printed over console
    
    * Merge branch 'development/1.0' into SIT-7-implement-tree-view-functionality-tree-completion

--- src/app/components/homepage/editor/editor.component.ts
+++ src/app/components/homepage/editor/editor.component.ts
@@ -36,7 +36,65 @@ export class EditorComponent implements OnInit {
       this.bDisplayAlert = false
     }, 3000);
   }
- 
+  alphafunction(sIncommingTextArea : any)
+  {
+    let nStartPosition = sIncommingTextArea.selectionStart;
+    let nEndPosition = sIncommingTextArea.selectionEnd;
+    if(nStartPosition == nEndPosition)
+    {
+      let startSubStr : string = sIncommingTextArea.value.substring(0, nStartPosition)
+      let endSubStr: string = sIncommingTextArea.value.substring(nStartPosition, sIncommingTextArea.value.length)
+      let selectedWord = "";
+      if(startSubStr.lastIndexOf('|') > startSubStr.lastIndexOf('^')){
+        endSubStr=endSubStr+"|^";
+        if(endSubStr.indexOf('|') > endSubStr.indexOf('^')){
+          selectedWord= startSubStr.substring(startSubStr.lastIndexOf('|')+1, startSubStr.length) + endSubStr.substring(0, endSubStr.indexOf('^'))
+        }
+        else{
+          selectedWord= startSubStr.substring(startSubStr.lastIndexOf('|')+1, startSubStr.length) + endSubStr.substring(0, endSubStr.indexOf('|'))
+        }
+
+      }
+      // -----------------------------------
+      else{
+        endSubStr=endSubStr+"|^";
+        if(endSubStr.indexOf('|') > endSubStr.indexOf('^')){
+          selectedWord= startSubStr.substring(startSubStr.lastIndexOf('^')+1, startSubStr.length) + endSubStr.substring(0, endSubStr.indexOf('^'))
+        }
+        else{
+          selectedWord= startSubStr.substring(startSubStr.lastIndexOf('^')+1, startSubStr.length) + endSubStr.substring(0, endSubStr.indexOf('|'))
+        }
+
+      }
+      if(selectedWord.search('\n')>0)
+        selectedWord=selectedWord.substring(0,selectedWord.indexOf('\n'))
+      console.log("final word=", selectedWord);
+      
+      // -----------------------------------
+      
+
+      let header:string ='';
+      if(startSubStr.lastIndexOf('\n') < startSubStr.indexOf('|'))
+      {
+        header = startSubStr.substring(startSubStr.lastIndexOf('\n') , startSubStr.indexOf('|')).trim()
+      }
+      else
+      {
+        header = sIncommingTextArea.value.substring(startSubStr.lastIndexOf('\n'), endSubStr.indexOf('|')+startSubStr.length).trim()
+        header = header.substring(0, header.indexOf('|')).trim()
+      }
+      if(header==''){
+        header = selectedWord.trim();
+      }
+      console.log("header =",header)
+    }
+    else
+    {
+      console.log("Selected text from ("+ nStartPosition +" to "+ nEndPosition + " of " + sIncommingTextArea.value.length + ")");
+      console.log(sIncommingTextArea.value.substring(nStartPosition,nEndPosition));
+
+    }
+  }
   ResizePanels()
   {
     let chatScript = document.createElement("script");
