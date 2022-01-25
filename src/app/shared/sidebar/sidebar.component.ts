97a2f4df8cc84155682826d8d25b604393c36208
Usman Ali <usman.ali@zaxiss.com>
2022-01-25 20:31:44 +0500

    implemented downloaded txt file feature

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,8 +1,10 @@
 import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { DataService } from 'src/app/services/data.service';
 import * as HL7Inspector from '../../../assets/standard_profiles/HL7InspectorNEO-HL7_V2.5.1-Profile.json';
 import * as HL7Inspector26 from '../../../assets/standard_profiles/HL7InspectorNEO-HL7_V2.6-Profile.json';
+import { saveAs as UploadFilesComponent_DownloadResultAsJSON }  from 'save-as';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,8 +12,6 @@ import * as HL7Inspector26 from '../../../assets/standard_profiles/HL7InspectorN
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-  // sidebarOptions : any = ['ADT^A08 with UTF8 Sample','ADT^A01 Sample','ORU^R01 with PDF Sample','ADT^A02 Sample','MDM^T02 with JPG Sample',]
-  addActiveClass: any;
   selectedTheme: any = "";
   bDisplayPreviewPanel = false
   sOverlay : string = ""
@@ -23,46 +23,40 @@ export class SidebarComponent implements OnInit {
   sDisplayWord : string;
   bDisplayPDFError : boolean = false;
   bDisplayImageError : boolean = false;
-  constructor(private oDataService : DataService, private oDatePipe : DatePipe) { }
+  constructor(private oDataService : DataService, private oDatePipe : DatePipe, private oSanitizer: DomSanitizer) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void 
+  {
     this.selectedTheme = localStorage.getItem('selectedTheme')
     this.oDataService.oWordToSearch.subscribe(data=>
     {
       console.log("Incomming word : ==> ", data);
       if(data.header!=="")
       {
-        this.alphaFunction(data);
+        this.SidebarComponent_ExtractHeaderDetails(data);
         this.bDisplayPDFError = false;
         this.bDisplayImageError = false;
       }
     })
   }
-  AddActiveClass(item:any){
-    console.log("Item", item);
-    this.addActiveClass = item
-  }
 
-  DisplayPreviewModal()
+  SidebarComponent_DisplayPreviewModal()
   {
     this.bDisplayPreviewPanel = true;
     this.sOverlay = "overlay-fade";
   }
-  DisplaypdfFile()
+  SidebarComponent_DisplaypdfFile()
   {
-    console.log("work");
-    
     this.bDisplayPdfPanal = true;
     this.sOverlay = "overlay-fade";
   }
-  
-  DismissPreviewPanel()
+  SidebarComponent_DismissPreviewPanel()
   {
     this.bDisplayPreviewPanel = false;
     this.bDisplayPdfPanal = false;
     this.sOverlay = '';
   }
- alphaFunction(oIncommingData : any)
+  SidebarComponent_ExtractHeaderDetails(oIncommingData : any)
  {
   this.sSelectedWord = oIncommingData.word;
   this.sDisplayWord = JSON.parse(JSON.stringify(this.sSelectedWord));
@@ -87,42 +81,53 @@ export class SidebarComponent implements OnInit {
     if(this.lSegments[nIndex].seg.seg === oIncommingData.header)
     {
       this.oSelectedSegment = this.lSegments[nIndex].seg;
+      console.log("Selected Segment ===>>>",this.oSelectedSegment);
       break;
     }
   }
-  console.log("Selected Segment ===>>>",this.oSelectedSegment);
  }
- ConvertIntoDateTime()
+ SidebarComponent_ConvertIntoDateTime()
  {
   this.sDisplayWord = this.oDatePipe.transform(this.sSelectedWord,'EEE, d MMM y hh:mm:ss')
  }
- DisplayImageError()
+ SidebarComponent_DisplayImageError()
  {
    this.bDisplayImageError = true;
  }
- DisplayPDFError()
+ SidebarComponent_DisplayPDFError()
  {
    this.bDisplayPDFError = true;
  }
- CopyToClipBoard()
+ SidebarComponent_CopyToClipBoard()
  {
   navigator.clipboard.writeText(this.sSelectedWord);
   console.log(navigator.clipboard.writeText(this.sSelectedWord));
  }
- DownloadFile()
+ SidebarComponent_DownloadFile()
  {
    if(this.bDisplayPDFError)
    {
-     //download as a image file
+    console.log("//download as a image file")
    }
    if(this.bDisplayImageError)
-   {
-     //download as a pdf file
+   {   
+     console.log("//download as a pdf file")
    }
    else
    {
+     console.log("//download as a text file")
      //download as a text file
+    //  const blob = new Blob([this.sSelectedWord], { type: 'application/octet-stream' });
+    //  let url : any = this.oSanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob));
+    //  const link = document.createElement('a');
+    //  link.href = url;
+    //  link.setAttribute('download', 'message.txt');
+    //  document.body.appendChild(link);
+    //  link.click();
+    //  document.body.removeChild(link);
 
+     let oJobResults = new Blob([JSON.stringify(this.sSelectedWord, null, 2)], { type: 'text;charset=utf-8' })
+     UploadFilesComponent_DownloadResultAsJSON(oJobResults, 'message.txt');
    }
  }
 }
