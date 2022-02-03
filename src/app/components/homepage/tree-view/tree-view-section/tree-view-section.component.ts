5ef20e4003f3638668bf3ee955e5506f6db47b7d
Usman Ali <usman.ali@zaxiss.com>
2022-02-03 22:35:55 +0500

    Tree view in progress

--- src/app/components/homepage/tree-view/tree-view-section/tree-view-section.component.ts
+++ src/app/components/homepage/tree-view/tree-view-section/tree-view-section.component.ts
@@ -1,272 +1,31 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
 import { DataService } from 'src/app/services/data.service';
-declare var require: any;
-export let sampleData: any[] = [
-  // 1
-  {
-    icon: true,
-    value:"MSH|^~\&amp;|ADT-HIS||HL7INSPECTOR||20060101103100||ADT^A02|1|P|2.5.1",
-    description: 'Message Header',
-    opt: '',
-    rp: "",
-    valuePresenter: false,
-    message: "The MSH segment defines the intent, source, destination, and some specifics of the syntax of a message.Chapter: 2.15.9",
-    subtasks: [
-      { value:"1: |" , description: 'Field Seperator',opt: 'R',valuePresenter: true },
-      { value:"2: |",  description: 'Encoding Character', opt:'R' ,valuePresenter: true},
-      { icon: true , value:"3: ADT-HIS",  description: 'Sending Application', opt:'O',valuePresenter: true,
-        subtasks: [
-          { value:"ADT-HIS" , description: 'Namespace ID', opt:'O' , rp:'',valuePresenter: true}
-        ]
-      },
-       ]
-  },
-  // 2
-  {
-    icon: true,
-    value: 'EVN|A01|20060101||',
-    description: 'Event Type',
-    opt:'',
-    rp:'',
-    valuePresenter: false,
-    message: 'The EVN segment is used to communicate necessary trigger event information to receiving applications.Chapter: 3.4.1',
-    subtasks: [
-      { value: '1: A01',  description: 'Event Type Code', opt:'B' ,valuePresenter: true},
-      { value: '2: 20060101', description: 'Recorded Date/Time' , opt:'R' ,valuePresenter: true },
-       ]
-  },
-  // 3
-  {
-    icon: true,
-    value:"PID||||4711|母鹿^约翰~Doe^John||19700220|M|||Sesamstreet 17^76137^上海^CN|||||GS|EV||||	",
-    description: 'Patient / Identification',
-    opt: '',
-    rp:'',
-    valuePresenter: false,
-    message:"The PID segment is used by all applications as the primary means of communicating patient identification information. This segment contains permanent patient identifying and demographic information that, for the most part, is not likely to change frequently.Chapter: 3.4.2",
-    subtasks: [{ icon: true , value: '1: 1', description: 'Set ID - OBX', opt: 'R',valuePresenter: true,
-          subtasks: [{
-           value: '1',
-           description: 'Sequence ID',
-           opt: '',
-           rp:'',
-           valuePresenter: true
-        }]
-      },
-      {
-        icon: true,
-        label : true ,
-        value: '2: ED',
-        description: 'Value Type',
-        opt: 'R',
-        rp:'',
-        valuePresenter: true,
-        subtasks: [{
-          icon: false,
-          label : true ,
-          value: 'ED ',
-          description: 'Coded Value for HL7-Defined Tables',
-          opt: '',
-          rp:'',
-          valuePresenter: true,
-        }]
-      },
 
-      {
-        icon: true,
-        value: '3: EGK_DATA52^eGK-Daten^HL7-DEU',
-        description: 'Observation Identifier',
-        opt: 'O',
-        rp:'',
-        valuePresenter: true,
-        subtasks: [{
-          icon: false,
-          value: 'EGK_DATA52	',
-          description: 'Identifier',
-          opt: 'O',
-          rp:'',
-          valuePresenter: true
-        },
-        {
-          icon: false,
-          value: 'eGK-Daten',
-          description: 'Text',
-          opt: 'O',
-          rp:'',
-          valuePresenter: true
-        },
-        {
-          icon: false,
-          value: 'HL7-DEU',
-          description: 'Name of Coding System',
-          opt: 'O',
-          rp:'',
-          valuePresenter: true
-        }]
-      }
-    ]
-  },
-   // 4
-   {
-    icon: true,
-    value:"OBX|3|ED|502^CHEST XRAY^L||Word^TEXT^^Base64^SnVzdCBhIHNpbXBsZSB0ZXh0",
-    description: 'Observation / Result',
-    opt: '',
-    rp:'',
-    valuePresenter: false,
-    subtasks: [{ icon: true , value: '1: 3', description: 'Set ID - OBX', opt: 'R', valuePresenter: true,
-          subtasks: [{
-            id:19 ,
-           value: '3',
-           description: 'Sequence ID',
-           opt: '',
-           rp:'',
-           valuePresenter: true
-        }]
-      },
-      {
-        icon: true,
-        label:true,
-        value: '2: ED',
-        description: 'Value Type',
-        opt: 'R',
-        rp:'',
-        valuePresenter: true,
-        subtasks: [{
-          icon: false,
-          label : true ,
-          value: 'ED ',
-          description: 'Coded Value for HL7-Defined Tables',
-          opt: '',
-          rp:'',
-          valuePresenter: true
-        }]
-      },
-
-      {
-        icon: true,
-        value: '3: 502^CHEST XRAY^L',
-        description: 'Observation Identifier',
-        opt: 'O',
-        rp:'',
-        valuePresenter: true,
-        subtasks: [{
-          icon: false,
-          value: '502',
-          description: 'Identifier',
-          opt: 'O',
-          rp:'',
-          valuePresenter: true
-        },
-        {
-          icon: false,
-          value: 'CHEST XRAY',
-          description: 'Text',
-          opt: 'O',
-          rp:'',
-          valuePresenter: true
-        },
-        {
-          icon: false,
-          value: 'L',
-          description: 'Name of Coding System',
-          opt: 'O',
-          rp:'',
-          valuePresenter: true
-        }]
-      },
-      {
-        icon: true,
-        value: '5: Word^TEXT^^Base64^SnVzdCBhIHNpbXBsZSB0ZXh0',
-        description: 'Observation Value',
-        opt: 'C/R',
-        rp:'refresh',
-        valuePresenter: true,
-        subtasks: [{
-          icon: false,
-          value: 'Word',
-          description: '',
-          opt: '',
-          rp:'',
-          valuePresenter: true
-        },
-        {
-          icon: false,
-          value: 'TEXT',
-          description: '',
-          opt: '',
-          rp:'',
-          valuePresenter: true
-        },
-        {
-          icon: false,
-          value: 'L',
-          description: 'Name of Coding System',
-          opt: 'O',
-          rp:'',
-          valuePresenter: true
-        }]
-      }
-    ]
-  }
-];
 @Component({
   selector: 'app-tree-view-section',
   templateUrl: './tree-view-section.component.html',
   styleUrls: ['./tree-view-section.component.scss']
 })
 export class TreeSiewSectionComponent implements OnInit, OnDestroy {
-  pdfSrc = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf";
   bDisplayAlert: boolean = false;
   sText: string = "";
-  sampleData = sampleData;
-  items: any = [];
   messageHeaderPane: any = "No segement selected";
   badgeData: any;
   message: any;
   pane1 : any = "No file selected or unknown"
   fileUrl: any = "";
-  ValuePresenterDisplayOptions : any = [
-    { name: 'Original Value' , value: 'original'},
-    { name: 'Formatted Value' , value: 'formatted'},
-    { name: 'Formatted Date / Time' , value: 'datetime'},
-    { name: 'Decoded Base64 String' , value: 'base64'},
-    { name: 'Decoded Base64 PDF Document' , value: 'base64-pdf'},
-    { name: 'Decoded Base64 Image' , value: 'base64-image'},
-    { name: 'Decoded Base64 XML Document' , value: 'base64-xml'},
-    { name: 'Decoded Base64 CDA Document (Experimental)' , value: 'base64-cda'}
-  ]
-  base_64_pdf: boolean = false;
-  base64_xml: boolean = false;
-  selectedValue: any;
-  app = require( '@3dgenomes/ngx-resizable/package.json');
-  title: string = this.app.name;
-  version: string = this.app.version;
-  bDisplaySelectedOption: boolean;
-  bDisplayChild1: boolean = false;
-  bDisplayChild2: boolean = false;
-  bDisplayChild3: boolean = false;
-  bDisplayChild4: boolean = false;
-  bDisplayChild5: boolean = false;
-  bDisplayChild6: boolean = false;
-  bDisplayChild7: boolean = false;
-  bDisplayChild8: boolean = false;
-  bDisplayChild9: boolean = false;
 
   oIncommingTextSubscription : any;
   sIncommingText : string;
   lFirstLevelNesting : any = [];
   lSecondLevelNesting : any = [];
   lThirdLevelNesting : any = [];
-  constructor(private sanitizer: DomSanitizer, private oDataService : DataService) { }
+
+
+
+  constructor( private oDataService : DataService) { }
   
   ngOnInit(): void {
-    const data = 'some text';
-    const blob = new Blob([data], { type: 'application/octet-stream' });
-    
-    this.fileUrl = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob));
-    this.items=this.getItems(this.sampleData,null,0)
     this.TreeSiewSectionComponent_DrawTreeView();
   }
   ngOnDestroy(){this.oIncommingTextSubscription.unsubscribe();}
@@ -278,65 +37,39 @@ export class TreeSiewSectionComponent implements OnInit, OnDestroy {
       this.lFirstLevelNesting = this.sIncommingText.split('\n');
       for (let nTreeNodeIndex = 0; nTreeNodeIndex < this.lFirstLevelNesting.length; nTreeNodeIndex++) 
       {
-        const first = this.lFirstLevelNesting[nTreeNodeIndex];
-        
+        var obj = { parentNode : [], childNodes : [], grandChildNodes : []};
+        const parent = this.lFirstLevelNesting[nTreeNodeIndex];
+        obj.parentNode = parent;
+        const child = this.splitBasedOnBar(parent);
+        obj.childNodes = child;
+        for (let nGrandChildIndex = 0; nGrandChildIndex < child.length; nGrandChildIndex++) 
+        {
+          const currentChild = child[nGrandChildIndex];
+          const grandChild = this.splitBasedOnCap(currentChild);
+          obj.grandChildNodes = grandChild;
+        }
+
+        this.lSecondLevelNesting.push(obj);
       }
-      console.log("First level Nesting : ==> ",this.lFirstLevelNesting);
+      console.log("First level Nesting + Second Level Nesting : ==> ",this.lSecondLevelNesting);
       
     })
   }
-  splitBasedOnBar(sIncommingText : string)
+  splitBasedOnBar(sIncommingText : any)
   {
-    console.log("Selected text : ==>", sIncommingText);
-    let split = sIncommingText.split('|');
-    console.log("Text after split  : ==>", split);
+    // console.log("Selected text : ==>", sIncommingText);
+    return sIncommingText.split('|');
+    // console.log("Text after split  : ==>", split);
 
   }
-  splitBasedOnCap(sIncommingText : string)
+  splitBasedOnCap(sIncommingText : any)
   {
-    console.log("Selected text : ==>", sIncommingText);
-    let split = sIncommingText.split('^');
-    console.log("Text after split  : ==>", split);
+    // console.log("Selected text : ==>", sIncommingText);
+    return sIncommingText.split('^');
+    // console.log("Text after split  : ==>", split);
 
   }
-  expanded(item:any)
-  {
-    item.expanded=!item.expanded;
-    this.items=this.getItems(this.sampleData,null,0);
-  }
-  getItems(data:any, items:any,index:any) {
-    data.forEach((x:any) => {
-      if (!items)
-        items=[];
-      items.push(x);
-      items[items.length-1].index=index
-      if (x.subtasks && x.expanded)
-        this.getItems(x.subtasks,items,index+1);
-    }
-    )
-    return items;
-  }
-  ReadValues(item:any)
-  {
-    console.log("item",item);
-    if(!item.valuePresenter)
-    {
-      this.messageHeaderPane = item.description;
-      this.message = item.message
-    }
-    else 
-    {
-      this.pane1 = item.description
-    }
-    if(item.valuePresenter)
-    {
-      this.badgeData = item.value
-    }
-    else
-    {
-      this.badgeData = ""
-    }
-  }
+
   CopyToClipBoard()
   {
     this.bDisplayAlert = true
@@ -345,52 +78,5 @@ export class TreeSiewSectionComponent implements OnInit, OnDestroy {
       this.bDisplayAlert = false
     }, 3000);
   }
-  Compared()
-  {
-    this.bDisplayAlert = true
-    this.sText = "Successful Compared. See the results in compare tab"
-    setTimeout(() => {
-      this.bDisplayAlert = false
-    }, 3000);
-  }
-  SelectDisplayOptions(options:any) 
-  {
-    this.selectedValue = options.target.value
-    console.log(this.selectedValue);
-    
-  }
-  DisplayChild1()
-{
-  this.bDisplayChild1 =  !this.bDisplayChild1
-}
-DisplayChild2()
-{
-  this.bDisplayChild2 =  !this.bDisplayChild2
-}DisplayChild3()
-{
-  this.bDisplayChild3 =  !this.bDisplayChild3
-}
-DisplayChild4()
-{
-  this.bDisplayChild4 =  !this.bDisplayChild4
-}
-
-DisplayChild5()
-{
-  this.bDisplayChild5 =  !this.bDisplayChild5
-}
-DisplayChild6()
-{
-  this.bDisplayChild6 =  !this.bDisplayChild6
-}DisplayChild7()
-{
-  this.bDisplayChild7 =  !this.bDisplayChild7
-}DisplayChild8()
-{
-  this.bDisplayChild8 =  !this.bDisplayChild8
-}DisplayChild9()
-{
-  this.bDisplayChild9 =  !this.bDisplayChild9
-}
 
 }
