72e096eed41fb0c2e0a0aac33bf44e2b1fcacfa6
Jesse Dowell <jesse.dowell@gmail.com>
2022-05-06 17:50:10 -0400

    prod environment converter urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  enableCrsfTokenRequest : false,
+  hl7JavaConvertUrl: '/hl7-tool/api/java/hl72fhir',
+  hl7CsharpConvertUrl: '/hl7-tool/api/csharp/convert',
 };
