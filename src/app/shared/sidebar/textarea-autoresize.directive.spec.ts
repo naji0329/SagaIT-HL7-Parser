c43c91ea910280fe8ab2cb7c218f331c224d7cd7
Jesse Dowell <jesse.dowell@gmail.com>
2022-05-18 15:03:48 -0400

    fixed a few warnings, still issues

--- src/app/shared/sidebar/textarea-autoresize.directive.spec.ts
+++ src/app/shared/sidebar/textarea-autoresize.directive.spec.ts
@@ -2,7 +2,7 @@ import { TextareaAutoresizeDirective } from './textarea-autoresize.directive';
 
 describe('TextareaAutoresizeDirective', () => {
   it('should create an instance', () => {
-    const directive = new TextareaAutoresizeDirective();
+    const directive = new TextareaAutoresizeDirective(null);
     expect(directive).toBeTruthy();
   });
 });
